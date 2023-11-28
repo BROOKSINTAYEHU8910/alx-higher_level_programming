20e3a160edb37ac9cbd4f010fcc46d81d2e14327
Brook <sintayehubrook1540@gmail.com>
2023-11-28 15:56:12 -0800

    first

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,22 +1,7 @@
-#!/usr/bin/python3
-
 def print_last_digit(number):
-    number = abs(number)
-    last_digit = number % 10
-    print(last_digit)
-    return last_digit
-
-def test_print_last_digit():
-    output = print_last_digit(98)
-    assert output == 8, f"Expected: 8, Got: {output}"
-
-    output = print_last_digit(98)
-    assert output == 8, f"Expected: 8, Got: {output}"
-
-    output = print_last_digit(-98)
-    assert output == 8, f"Expected: 8, Got: {output}"
-
-    output = print_last_digit(0)
-    assert output == 0, f"Expected: 0, Got: {output}"
-
-test_print_last_digit()
+    if number < 0:
+        last_num = (-number % 10)
+    elif number >= 0:
+        last_num = number % 10
+    print("{:d}".format(last_num), end="")
+    return last_num
