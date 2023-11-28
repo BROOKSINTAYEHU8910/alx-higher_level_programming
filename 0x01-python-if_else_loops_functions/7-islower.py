c25420518074ed4443c63342337be956ae9966b9
Brook <sintayehubrook1540@gmail.com>
2023-11-28 15:18:39 -0800

    first

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    # Check if the ASCII value of the character is between 97 and 122
-    return ord(c) >= 97 and ord(c) <= 122
+    return 'a' <= c <= 'z'
 
 # Test cases
 print("a is {}".format("lower" if islower("a") else "upper"))
@@ -9,4 +8,3 @@ print("H is {}".format("lower" if islower("H") else "upper"))
 print("A is {}".format("lower" if islower("A") else "upper"))
 print("3 is {}".format("lower" if islower("3") else "upper"))
 print("g is {}".format("lower" if islower("g") else "upper"))
-
