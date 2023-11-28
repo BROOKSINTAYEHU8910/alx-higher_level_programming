52a4a3d0dc5d2beb933bc1fcd12da9c15ad4d0e4
Brook <sintayehubrook1540@gmail.com>
2023-11-28 12:50:26 -0800

    first

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for i in range(10):
     for j in range(i + 1, 10):
         if i != j:
-            separator = ", " if (i < 8 or (i == 8 and j == 9)) else "\n"
+            separator = ", " if (i < 8 or (i == 8 and j == 9)) else ""
             print("{:d}{:d}".format(i, j), end=separator)
-print()  # Add an additional newline to match the expected output
+print()  # Add a newline after the loops to match the expected output
