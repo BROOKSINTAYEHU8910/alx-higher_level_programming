ca867bc0ef96444eace0b4e8286b83c63ba012db
Brook <sintayehubrook1540@gmail.com>
2023-11-28 11:10:59 -0800

    first

---
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_alphabet():
+    for c in range(ord('a'), ord('z') + 1):
+        print("{}".format(chr(c)), end='')
+
+# Call the function to print the alphabet
+print_alphabet()
