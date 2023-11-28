7668486420a1fdb3308b4f8c0ac170c985bd10a0
Brook <sintayehubrook1540@gmail.com>
2023-11-28 15:52:39 -0800

    first

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,20 @@
-#!/usr/bin/python3
 def print_last_digit(number):
     number = abs(number)
     last_digit = number % 10
     print(last_digit)
     return last_digit
+
+def test_print_last_digit():
+    output = print_last_digit(98)
+    assert output == 8, f"Expected: 8, Got: {output}"
+
+    output = print_last_digit(98)
+    assert output == 8, f"Expected: 8, Got: {output}"
+
+    output = print_last_digit(-98)
+    assert output == 8, f"Expected: 8, Got: {output}"
+
+    output = print_last_digit(0)
+    assert output == 0, f"Expected: 0, Got: {output}"
+
+test_print_last_digit()
