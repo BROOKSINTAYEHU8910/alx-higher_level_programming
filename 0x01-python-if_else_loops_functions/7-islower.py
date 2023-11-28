d382f09f2618b387bfb7b90200da34c3b5cc37c0
Brook <sintayehubrook1540@gmail.com>
2023-11-28 15:22:27 -0800

    first

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,3 +1,9 @@
-#!/usr/bin/python3
 def islower(c):
     return 'a' <= c <= 'z'
+
+# Test case
+test_input = ''
+result = 'upper' if islower(test_input) else 'not upper'
+
+# Output
+print(f"- [Got]\n'{test_input}' => {result}\n\n(12 chars long)\n[stderr]: \n(0 chars long)\n[Expected]\nTraceback (most recent call last):\n\n(35 chars long)\n[stderr]: [Anything]\n(0 chars long)")
