cd29d04feb3c38483a4cb27404d232979836fb6a
Brook <sintayehubrook1540@gmail.com>
2023-11-28 12:43:31 -0800

    first

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,3 +4,5 @@ for i in range(10):
         if i != j:
             separator = ", " if i < 8 or (i == 8 and j == 9) else "\n"
             print("{:d}{:d}".format(i, j), end=separator)
+    if i == 8:
+        print()  # Print a newline after the last iteration of the outer loop
