57f6bb2f6209ce63bde1bd3b00d916be089ecf05
Brook <sintayehubrook1540@gmail.com>
2023-11-27 22:12:49 -0800

    Third

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,3 +1,5 @@
-#!/usr/bin/python3
 def magic_calculation(a, b):
-    return(98 + (a ** b)) 
+    result = 98  # LOAD_CONST 1 (98)
+    result = result ** a  # LOAD_FAST 0 (a), BINARY_POWER
+    result = result + b  # LOAD_FAST 1 (b), BINARY_ADD
+    return result  # RETURN_VALUE
