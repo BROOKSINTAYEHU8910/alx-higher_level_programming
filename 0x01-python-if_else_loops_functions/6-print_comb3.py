5d6de0edff390880545888e8d446b6105875fc86
Brook <sintayehubrook1540@gmail.com>
2023-11-28 12:52:37 -0800

    first

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for i in range(10):
+for i in range(9):
     for j in range(i + 1, 10):
         if i != j:
             separator = ", " if (i < 8 or (i == 8 and j == 9)) else ""
