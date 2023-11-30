bd9c32ebd272231768c2f0fc71b5da1a29e632a7
Brook <sintayehubrook1540@gmail.com>
2023-11-30 11:16:10 -0800

    finals

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import secret_module_42
+    import hidden_4
     # Print names sorted in alphabetical order
-    for name in sorted(dir(secret_module_42)):
+    for name in sorted(dir(hidden_4)):
         # Print only names that do not start with __
         if not name.startswith("__"):
             print("{}".format(name))
