b79874b874cd3743a963b5edcc13264bed5affb4
Brook <sintayehubrook1540@gmail.com>
2023-11-30 10:45:29 -0800

    final

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,24 +1,34 @@
 #!/usr/bin/python3
-import importlib.util
 import dis
+import py_compile
 import sys
+import tempfile
+import os
 
 def print_hidden_names(filename):
-    spec = importlib.util.spec_from_file_location("module_name", filename)
-    module = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(module)
+    # Create a temporary directory to store the compiled Python file
+    with tempfile.TemporaryDirectory() as temp_dir:
+        compiled_filename = os.path.join(temp_dir, "hidden_compiled.pyc")
 
-    # Extract names from the module
-    names = [name for name in dir(module) if not name.startswith('__')]
+        try:
+            # Compile the source code and save the compiled file
+            py_compile.compile(filename, cfile=compiled_filename)
+            with open(compiled_filename, 'rb') as file:
+                # Read the magic number and timestamp (8 bytes)
+                file.read(8)
+                # Read the rest of the file
+                file_contents = file.read()
 
-    # Print names in alphabetical order
-    for name in sorted(names):
-        print(name)
+            # Disassemble the bytecode
+            dis.dis(file_contents)
+        except SyntaxError as e:
+            print(f"Error compiling/disassembling {filename}: {e}")
+            sys.exit(1)
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: ./4-hidden_discovery.py <compiled_module.pyc>")
+        print("Usage: ./4-hidden_discovery.py <source_module.py>")
         sys.exit(1)
 
-    compiled_module_filename = sys.argv[1]
-    print_hidden_names(compiled_module_filename)
+    source_module_filename = sys.argv[1]
+    print_hidden_names(source_module_filename)
