34e69552d017f4ab6cbf731318195ab81c1950cc
Brook <sintayehubrook1540@gmail.com>
2023-11-30 11:02:12 -0800

    finals

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,22 +1,8 @@
 #!/usr/bin/python3
-import dis
-import importlib.util
-import sys
-
-def print_hidden_names(file_path):
-    spec = importlib.util.spec_from_file_location("hidden_4", file_path)
-    module = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(module)
-
-    names = [name for name in dir(module) if not name.startswith("__")]
-
-    for name in sorted(names):
-        print(name)
-
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: ./4-hidden_discovery.py <path to hidden_4.pyc>")
-        sys.exit(1)
-
-    file_path = sys.argv[1]
-    print_hidden_names(file_path)
+    import secret_module_42
+    # Print names sorted in alphabetical order
+    for name in sorted(dir(secret_module_42)):
+        # Print only names that do not start with __
+        if not name.startswith("__"):
+            print("{}".format(name))
