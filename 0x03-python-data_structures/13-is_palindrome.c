d05bf8ed124fe129cb144b1ea394a6390e52818b
Brook <sintayehubrook1540@gmail.com>
2023-12-03 00:24:01 -0800

    third

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -3,62 +3,51 @@
 #include <stdio.h>
 
 /**
- * is_palindrome - Identify if a singly linked list is a palindrome.
- * @head: Pointer to the head of the list.
- * Return: 1 if it is a palindrome, 0 otherwise.
- */
+*add_nodeint - adds a new node at the beginning of a listint_t list
+*@head: head of listint_t
+*@n: int to add in listint_t list
+*Return: address of the new element, or NULL if it failed
+*/
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = *head;
+	*head = new;
+	return (new);
+}
+/**
+*is_palindrome - identify if a syngle linked list is palindrome
+*@head: head of listint_t
+*Return: 1 if it is palindrome else 0
+*/
 int is_palindrome(listint_t **head)
 {
-    listint_t *slow = *head, *fast = *head;
-    listint_t *reversed = NULL, *temp = NULL;
+	listint_t *head2 = *head;
+	listint_t *aux = NULL, *aux2 = NULL;
 
-    if (*head == NULL || (*head)->next == NULL)
-        return (1);
-
-    // Move slow and fast pointers to find the middle
-    while (fast != NULL && fast->next != NULL)
-    {
-        // Update reversed list
-        temp = reversed;
-        reversed = malloc(sizeof(listint_t));
-        if (reversed == NULL)
-        {
-            // Handle memory allocation failure
-            free_listint(reversed);
-            return (0);
-        }
-        reversed->n = slow->n;
-        reversed->next = temp;
-
-        slow = slow->next;
-        fast = fast->next->next;
-    }
-
-    // If the length is odd, move slow one step forward
-    if (fast != NULL)
-        slow = slow->next;
-
-    // Compare the first half and the reversed second half
-    while (slow != NULL)
-    {
-        if ((*head)->n != reversed->n)
-        {
-            // Clean up and return 0 if not a palindrome
-            free_listint(reversed);
-            return (0);
-        }
-
-        *head = (*head)->next;
-        temp = reversed;
-        reversed = reversed->next;
-        free(temp);
-
-        slow = slow->next;
-    }
-
-    // Clean up reversed list
-    free_listint(reversed);
-
-    // It is a palindrome
-    return (1);
+	if (*head == NULL || head2->next == NULL)
+		return (1);
+	while (head2 != NULL)
+	{
+		add_nodeint(&aux, head2->n);
+		head2 = head2->next;
+	}
+	aux2 = aux;
+	while (*head != NULL)
+	{
+		if ((*head)->n != aux2->n)
+		{
+			free_listint(aux);
+			return (0);
+		}
+		*head = (*head)->next;
+		aux2 = aux2->next;
+	}
+	free_listint(aux);
+	return (1);
 }
